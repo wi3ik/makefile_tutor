e177fef05c67f1fe0c588e3c5ddb29ebf331a965
Vitaliy Kharechko <vitaly.kharechko@gmail.com>
2019-09-11 00:48:29 +0300

    Added example of pathing binary file on Mac

---
+++ path_for_binary/binary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int a = 10;
+
+  if (a < 11) {
+    printf("We see excpected log message.\n");
+  }
+ 
+  return 0;
+}
---
+++ path_for_binary/patch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  FILE *f = fopen("binary", "r+b");
+  unsigned int opcode = 0x118c0f; // order should need reverted, 8d (jge) => 8c (jl) [ on Mac ]
+
+/*
+100000f51:	0f 8d 11 00 00 00 	jge	17 <_main+0x38>
+                V
+100000f51:	0f 8c 11 00 00 00 	jl	17 <_main+0x38>
+*/
+
+  fseek(f, 0xf51, SEEK_SET);
+
+  fwrite(&opcode, sizeof(opcode), 1, f);
+
+  fclose(f);
+
+  return 0;
+}
